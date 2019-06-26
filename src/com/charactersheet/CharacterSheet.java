1a54eb1e609aecc21d47886ed35ed0f61d69a66d
Billy <billymcfly@gmail.com>
2019-06-25 21:37:08 -0600

    Add file loading and saving

--- src/com/charactersheet/CharacterSheet.java
+++ src/com/charactersheet/CharacterSheet.java
@@ -1,8 +1,10 @@
 package com.charactersheet;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class CharacterSheet {
     private JButton btnSave;
@@ -13,13 +15,44 @@ public class CharacterSheet {
     private JPanel pnlCharacterInfo;
     private JButton btnOpen;
 
+    private CharacterSheetService characterSheetService;
+    private final FileNameExtensionFilter filter = new FileNameExtensionFilter(
+            "JSON file", "json");
+
     public CharacterSheet() {
+        CharacterModel characterModel = new CharacterModel();
+        characterSheetService = new CharacterSheetService(characterModel);
+
+        // Save
         btnSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hello World");
+                File file = openFile();
+                characterSheetService.saveToFile(file);
             }
         });
+
+        // Open
+        btnOpen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File file = openFile();
+                characterSheetService.readFromFile(file);
+            }
+        });
+    }
+
+    private File openFile()
+    {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(filter);
+        File file = null;
+        if (fileChooser.showOpenDialog(panelMain) == JFileChooser.APPROVE_OPTION) {
+
+            file = fileChooser.getSelectedFile();
+        }
+
+        return file;
     }
 
     public static void main(String[] args) {
--- src/com/charactersheet/CharacterSheetService.java
+++ src/com/charactersheet/CharacterSheetService.java
@@ -1,5 +1,8 @@
 package com.charactersheet;
 
+import java.io.*;
+import java.nio.file.Files;
+
 public class CharacterSheetService {
     private CharacterModel characterModel;
     private com.google.gson.Gson gson;
@@ -15,5 +18,26 @@ public class CharacterSheetService {
     public CharacterModel loadFromJSON(String strJson) {
         CharacterModel outputCharacterModel = gson.fromJson(strJson, CharacterModel.class);
         return outputCharacterModel;
+    }
+
+    public void saveToFile(File file) {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("writing to file");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readFromFile(File file) {
+        String test = new String();
+        try {
+            test = Files.readString( file.toPath() );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(test);
     }
 }
